ab7d01e21911e7c7d1cf89d510f9be8c587ab642
vaibhavmantri <vaibhavmantri824228@gmail.com>
2022-08-29 14:50:49 +0530

    processing

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home'
 function App() {
   return (
     <div className="App">
       <h1>Learning Git</h1>
+      <Home />
     </div>
   );
 }
